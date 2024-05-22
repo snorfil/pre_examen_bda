9d6dfd90c44c40868edfd516c4522c5185ba15d8
Edwin Leandro Montoya Osorio <emontoya@sdi.es>
2024-05-22 12:22:57 +0200

    neo4j corregido

--- apps/neo4j_kafka_consumer.py
+++ apps/neo4j_kafka_consumer.py
@@ -14,25 +14,34 @@ esquema = StructType([
     StructField("alergenos", StringType(), True)
 ])
 
-# Crear sesión de Spark
+aws_access_key_id = 'test'
+aws_secret_access_key = 'test'
+
 spark = SparkSession.builder \
-    .appName("KafkaSparkConsumer") \
-    .config("spark.sql.shuffle.partitions", 4) \
+    .appName("neo4j kafka") \
     .config("spark.streaming.stopGracefullyOnShutdown", "true") \
-    .config("spark.sql.streaming.schemaInference", "true") \
-    .config("spark.hadoop.fs.s3a.endpoint", "http://localhost:4566") \
-    .config("spark.hadoop.fs.s3a.access.key", "test") \
-    .config("spark.hadoop.fs.s3a.secret.key", "test") \
+    .config("spark.sql.shuffle.partitions", 2) \
+    .config("spark.hadoop.fs.s3a.endpoint", "http://spark-localstack-1:4566") \
+    .config("spark.hadoop.fs.s3a.access.key", aws_access_key_id) \
+    .config("spark.hadoop.fs.s3a.secret.key", aws_secret_access_key) \
+    .config("spark.jars.packages",
+            "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.1,"
+            "org.apache.kafka:kafka-clients:2.8.0,"
+            "software.amazon.awssdk:s3:2.25.11,"
+            "org.apache.commons:commons-pool2:2.11.1,"
+            "org.apache.kafka:kafka_2.12:2.8.0") \
     .config("spark.hadoop.fs.s3a.path.style.access", "true") \
     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-    .master("local[*]") \
+    .config("spark.driver.extraClassPath", "/opt/spark/jars/*") \
+    .config("spark.executor.extraClassPath", "/opt/spark/jars/*") \
+    .master("spark://spark-master:7077") \
     .getOrCreate()
 
 # Leer el stream de Kafka
 df = spark \
     .readStream \
     .format("kafka") \
-    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("kafka.bootstrap.servers", "kafka:9093") \
     .option("subscribe", "menus_stream") \
     .option("startingOffsets", "earliest") \
     .load()
@@ -45,12 +54,7 @@ df = df.selectExpr("CAST(value AS STRING)") \
 # Mostrar el esquema del DataFrame para depuración
 df.printSchema()
 
-# Iniciar la computación
-query = df \
-    .writeStream \
-    .outputMode("append") \
-    .format("console") \
-    .start()
+
 
 # Esperar a que termine la consulta
 query.awaitTermination()
